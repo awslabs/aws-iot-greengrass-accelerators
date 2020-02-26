a0e6b045afd1b93046dd6edf0159276e8a108c23
Gavin Adams <gavinaws@amazon.com>
2020-02-26 15:05:30 -0500

    flask app functionally complete

--- accelerators/stream_manager/docker_compose_stack/app/app.py
+++ accelerators/stream_manager/docker_compose_stack/app/app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+import json
+import time
 
-counter = 0
+import requests
+from datetime import datetime
+from flask import Flask, Response, render_template
+
+app = Flask(__name__, template_folder=".")
+app.debug = True
+app.testing = True
 
 
-@app.route('/')
-def hello():
-    global counter
-    counter += 1
-    return 'Hello World! I have been seen {} times.\n'.format(counter)
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/chart-data")
+def chart_data():
+    def get_sensor_data():
+        while True:
+            # Read latest 5 computed values from stream_aggregator Lambda
+            r = json.loads(requests.get("http://greengrass:8181/api/v1/aggregate").text)
+            json_data = json.dumps(
+                {
+                    "time": datetime.fromtimestamp(r["timestamp"]).strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                    "temperature": r["avg_temperature"],
+                    "hertz": r["avg_hertz"],
+                }
+            )
+            yield f"data:{json_data}\n\n"
+            time.sleep(5)
+
+    return Response(get_sensor_data(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    # port mapped via Docker compose
+    app.run(host="0.0.0.0", port=5000, debug=True)

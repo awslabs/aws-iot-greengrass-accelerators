55268310a5c3fa36b51e21e520866543565919fa
Gavin Adams <gavinaws@amazon.com>
2022-10-24 08:53:45 -0600

    build script test

--- scripts/v2_test_build.sh
+++ scripts/v2_test_build.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+
+# Functions used by script
+wait_str() {
+  local file="$1"; shift
+  local search_term="$1"; shift
+  local wait_time="${1:-5m}"; shift # 5 minutes as default timeout
+
+  (timeout $wait_time tail -F -n +1 "$file" &) | grep -q "$search_term" && return 0
+
+  echo "Timeout of $wait_time reached. Unable to find '$search_term' in '$file'"
+  return 1
+}
+
+wait_logfile() {
+  echo "Waiting for logfile..."
+  local server_log="$1"; shift
+  local wait_time="$1"; shift
+
+  wait_file "$server_log" "$wait_time" || { echo "Logfile file missing: '$server_log'"; return 1; }
+}
+
+wait_file() {
+  local file="$1"; shift
+  local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+
+  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; done
+
+  ((++wait_seconds))
+}
+
+# timeout script if running on macOS
+# Originally found on SO: http://stackoverflow.com/questions/601543/command-line-command-to-auto-kill-a-command-after-a-certain-amount-of-time
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+fi
+
 set -xe
 
 # verify running from (working directory) script dir
@@ -29,10 +65,16 @@ cdk deploy --require-approval "never" --ci
 
 # Builder docker container and start Greengrass (compose-up)
 cd $SCRIPT_DIR/../v2/base/docker
-
-
+python3 config_docker.py --clean
+python3 config_docker.py --use-envars
+docker-compose up -d
 
 # wait until greengrass.log stablizes (fully deployed)
+# wait_logfile $SCRIPT_DIR/../v2/base/docker/volues/gg_root/logs/greengrass.log 5m && \
+# echo -e "Received empty jobs in notification . {ThingName=gg-accel-base-greengrass-core"
+exit 0
+
+
 
 # Test `base` parts (helloworld output)
 
@@ -48,16 +90,13 @@ cd $SCRIPT_DIR/../v2/base/docker
 
 # cdk destroy etl_simple
 
-# docker-compose stop
-# clean docker volumes
+# docker-compose stop base
+cd $SCRIPT_DIR/../v2/base/docker
+docker-compose stop
+python3 config_docker.py --clean
+
+
 # destroy base
 cd $SCRIPT_DIR
 cd ../v2/base/cdk
 cdk destroy --force --ci
-
-# Error exit
-
-# if greengrass container running - docker-compose stop
-# clean docker volumes
-# if in os_cmd and cdk deployed, destroy os_cmd
-# cdk destroy base
